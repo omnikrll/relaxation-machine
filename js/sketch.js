04feef3b7217e5bd490343d1148fd2aa467da2a5
omnikrll <kfhohn@gmail.com>
2016-04-26 20:22:00 -0400

    most stuff is there

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,16 @@
-// audio params
-var samples = { l: null, r: null },
-	oscs = { l: null, r: null };
+// dom elements
+var textArea = document.getElementById('text-area'),
+	rateSlider = document.getElementById('rate-slider');
 
 // sphere rotation params
 var angle = 0,
-	speed = 0.015,
+	speed = rateSlider.value,
 	direction = 1;
 
-var textArea = document.getElementById('text-area'),
-	rateSlider = document.getElementById('rate-slider');
+// audio params
+var samples = { l: null, r: null },
+	oscs = { l: null, r: null },
+	playback = true;
 
 function preload() {
 	samples.l = loadSound('audio/audio_left.wav');
@@ -28,67 +30,75 @@ function startAudio() {
 	samples.l.setVolume(0.1)
 
 	samples.r.loop();
-	samples.r.pan(-0.4);
+	samples.r.pan(0.4);
 	samples.r.setVolume(0.1);
 
 	oscs.l = new p5.Oscillator();
 	oscs.l.setType('sine');
 	oscs.l.start();
-	oscs.l.freq(56);
+	oscs.l.freq(60);
 	oscs.l.pan(-1);
 	oscs.l.amp(0.2);
 
 	oscs.r = new p5.Oscillator();
 	oscs.r.setType('sine');
 	oscs.r.start();
-	oscs.r.freq(66);
+	oscs.r.freq(68);
 	oscs.r.pan(1);
 	oscs.r.amp(0.2);
 }
 
 function draw() {
-	speed = rateSlider.value;
+	if (playback) {
+		speed = rateSlider.value;
 
-	var sine = sin(angle),
-		cosine = cos(angle),
-		x = sine * 200,
-		y = cosine * 80, 
-		size_a = 50 + cosine * 30,
-		size_b = 50 + cosine * -30;
+		var sine = sin(angle),
+			cosine = cos(angle),
+			x = sine * 200,
+			y = cosine * 64, 
+			size_a = 56 + cosine * 32,
+			size_b = 56 + cosine * -32;
 
-	angle += speed * direction;
+		angle += speed * direction;
 
-	if (abs(ceil(sine)) == 0) {
-		textArea.innerHTML = 'exhale';
-	} else if (abs(ceil(sine)) == 1) {
-		textArea.innerHTML = 'inhale';
+		if (abs(ceil(sine)) == 0) {
+			textArea.innerHTML = 'exhale';
+		} else if (abs(ceil(sine)) == 1) {
+			textArea.innerHTML = 'inhale';
+		}
+
+		translate(x, y, 0);
+		push();
+		sphere(size_a);
+		pop();
+
+		translate((-x * 2), (-y * 2), 0);
+		push();
+		sphere(size_b);
+		pop();
 	}
-
-	translate(x, y, 0);
-	push();
-	sphere(size_a);
-	pop();
-
-	translate((-x * 2), (-y * 2), 0);
-	push();
-	sphere(size_b);
-	pop();
 }
 
 function keyPressed() {
 	if (keyCode == LEFT_ARROW) {
 		direction = -1;
-	} else if (keyCode == RIGHT_ARROW) {
+	}
+
+	if (keyCode == RIGHT_ARROW) {
 		direction = 1;
-	} else if (key == ' ') {
-		if (samples.l.isPlaying() && samples.r.isPlaying()) { 
-			samples.l.stop();
-			samples.r.stop();
+	}
+
+	if (key == ' ') {
+		if (playback) { 
+			playback = false;
+			samples.l.setVolume(0.0);
+			samples.r.setVolume(0.0);
 			oscs.l.amp(0);
 			oscs.r.amp(0);
-		} else if (!samples.l.isPlaying() && !samples.r.isPlaying()) {
-			samples.l.play();
-			samples.r.play();
+		} else {
+			playback = true;
+			samples.l.setVolume(0.1);
+			samples.r.setVolume(0.1);
 			oscs.l.amp(0.2);
 			oscs.r.amp(0.2);
 		}
